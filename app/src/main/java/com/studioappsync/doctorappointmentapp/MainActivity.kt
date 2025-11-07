80fec8f7d16d177a9faaf66fd723a9cffeac7f32
mirzamahmud <dev.mirzamahmud@gmail.com>
2025-11-08 00:58:29 +0600

    create repo

---
+++ app/src/main/java/com/studioappsync/doctorappointmentapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.studioappsync.doctorappointmentapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.studioappsync.doctorappointmentapp.ui.theme.DoctorAppointmentAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DoctorAppointmentAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, widthDp = 360, heightDp = 640)
+@Composable
+fun GreetingPreview() {
+    DoctorAppointmentAppTheme {
+        Greeting("Android")
+    }
+}
